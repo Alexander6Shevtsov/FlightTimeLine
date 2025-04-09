c46dab6fc81eaf3ad933b50702fc1ae120693366
Alexander6Shevtsov <axrsvr@gmail.com>
2025-04-09 18:37:29 +0300

    Some Fix

---
+++ FlightTimeLine/TimelineView.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineView.swift
+//  FlightTimeLine
+//
+//  Created by Alexander Shevtsov on 09.04.2025.
+//
+
+import SwiftUI
+
+struct TimelineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimelineView()
+}
