dec30e476eb3daf8c431f30a68e8b10daf2dec29
Alexander6Shevtsov <axrsvr@gmail.com>
2025-04-14 19:46:10 +0300

    Check Point

---
+++ FlightTimeLine/FlightBoardView.swift
@@ -0,0 +1,18 @@
+//
+//  FlightBoardView.swift
+//  FlightTimeLine
+//
+//  Created by Alexander Shevtsov on 11.04.2025.
+//
+
+import SwiftUI
+
+struct FlightBoardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlightBoardView()
+}
---
+++ FlightTimeLine/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  FlightTimeLine
+//
+//  Created by Alexander Shevtsov on 11.04.2025.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
