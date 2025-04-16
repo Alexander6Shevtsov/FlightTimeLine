119937bbf58ae40d399b5853af07101c651f3878
Alexander6Shevtsov <axrsvr@gmail.com>
2025-04-16 11:17:32 +0300

    Add FlightsDetails

---
+++ FlightTimeLine/FlightsDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  FlightsDetailsView.swift
+//  FlightTimeLine
+//
+//  Created by Alexander Shevtsov on 16.04.2025.
+//
+
+import SwiftUI
+
+struct FlightsDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FlightsDetailsView()
+}
