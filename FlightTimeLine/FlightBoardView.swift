7c4c181d91b4df61a1b7fce0374879cf52f60ef5
Alexander6Shevtsov <axrsvr@gmail.com>
2025-04-16 15:15:59 +0300

    Add Filter

--- FlightTimeLine/FlightBoardView.swift
+++ FlightTimeLine/FlightBoardView.swift
@@ -8,16 +8,29 @@
 import SwiftUI
 
 struct FlightBoardView: View {
+    @State private var hiddenCanceled = false // свойство фильтра
+    
     let boardName: String // через иниц
     let flights: [FlightInformation]
     
+    private var shownFlights: [FlightInformation] { // filtered массив
+        hiddenCanceled // перебираем массив
+        ? flights.filter { $0.status != .cancelled } // если статус не равен
+        : flights // иначе отображаем основной массив
+    }
+    
     var body: some View {
         //  ScrollView([.horizontal, .vertical]) { скролинг во све стороны
-        List(flights) { flight in // готовый список со скролом
+        List(shownFlights) { flight in // готовый список со скролом
             FlightRowView(flight: flight)
         }
         .listStyle(.plain)
         .navigationTitle(boardName)
+        .toolbar { // размещаем тулбар
+            ToolbarItem(placement: .topBarTrailing) { // задаем параметры
+                Toggle("Hide canceled", isOn: $hiddenCanceled) // кнопка
+            }
+        }
     }
 }
 
